a4436a8a3502306ac4bb58821c97141d443f8822
dmayman <dmaymandesign@gmail.com>
2024-12-18 18:01:40 -0800

    Create github_auto_pull.sh

---
+++ github_auto_pull.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Navigate to your repository
+cd /ninas
+
+# Pull the latest changes
+git pull origin main
+
+# Optional: Run a specific script after pulling (if needed)
+# ./your_script.sh
