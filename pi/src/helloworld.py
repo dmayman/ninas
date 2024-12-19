03eea082c703194da439de79c79313fb1f575049
dmayman <dmaymandesign@gmail.com>
2024-12-18 16:34:50 -0800

    afd

--- pi/src/helloworld.py
+++ pi/src/helloworld.py
@@ -11,8 +11,10 @@ GPIO.setup(LED_PIN, GPIO.OUT)
 try:
     while True:
         GPIO.output(LED_PIN, GPIO.LOW)  # Turn LED on (sinking current)
+        print("LED on")
         time.sleep(0.5)                # Wait 0.5 seconds
         GPIO.output(LED_PIN, GPIO.HIGH) # Turn LED off
+        print("LED off")
         time.sleep(0.5)                # Wait 0.5 seconds
 except KeyboardInterrupt:
     print("Exiting...")
