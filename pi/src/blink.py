8b94833518e1c79be301a7cda7c9af2f01598d76
dmayman <dmaymandesign@gmail.com>
2024-12-20 10:38:17 -0800

    17

--- pi/src/blink.py
+++ pi/src/blink.py
@@ -3,15 +3,15 @@ from time import sleep
 
 # Set up GPIO 0
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
 try:
     while True:
-        GPIO.output(0, GPIO.HIGH)  # Set GPIO 0 to HIGH
+        GPIO.output(17, GPIO.HIGH)  # Set GPIO 0 to HIGH
         print ("High")
         sleep(2)  # Wait for 1 second
         
-        GPIO.output(0, GPIO.LOW)   # Set GPIO 0 to LOW
+        GPIO.output(17, GPIO.LOW)   # Set GPIO 0 to LOW
         print ("Low")
         sleep(2)  # Wait for 1 second
 except KeyboardInterrupt:
