9f91c60bf1a47da03c959c4e8c3c6714d6f9df19
dmayman <dmaymandesign@gmail.com>
2024-12-27 14:29:44 -0800

    blink

--- pi/src/blink.py
+++ pi/src/blink.py
@@ -9,11 +9,11 @@ try:
     while True:
         GPIO.output(17, GPIO.HIGH)  # Set GPIO 0 to HIGH
         print ("High")
-        sleep(2)  # Wait for 1 second
+        sleep(.5)  # Wait for 1 second
         
         GPIO.output(17, GPIO.LOW)   # Set GPIO 0 to LOW
         print ("Low")
-        sleep(2)  # Wait for 1 second
+        sleep(.5)  # Wait for 1 second
 except KeyboardInterrupt:
     print("Exiting...")
 finally:
